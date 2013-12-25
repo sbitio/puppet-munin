a55a58c00b93d43069b3d7973de24bfeb0cc08c3
Pedro González Serrano <niteman.es@gmail.com>
2013-12-25 18:14:40 +0100

    added a method to bypass built-in plugins

--- manifests/node/autoconf.pp
+++ manifests/node/autoconf.pp
@@ -1,11 +1,20 @@
-class munin::node::autoconf () {
+class munin::node::autoconf (
+  $avoid = [],
+) {
 
   require munin::node::params
 
+  if $avoid = [] {
+    $filter = ''
+  }
+  else {
+    $filter = inline_template('<% avoid.each do |item| %>| sed "\@<%= item %>@d"<% end%>')
+  }
+
   exec {"munin-node-configure":
     #refreshonly => true,
-    command     => "munin-node-configure --shell | sh",
-    unless      => "[ $(munin-node-configure --shell 2> /dev/null | wc -l) = 0 ]",
+    command     => "munin-node-configure --shell ${filter} | sh",
+    unless      => "[ $(munin-node-configure --shell ${filter} 2> /dev/null | wc -l) = 0 ]",
     path        => ["/usr/bin", "/usr/sbin", "/bin"],
     notify      => Service[$munin::node::params::service_name],
   }
