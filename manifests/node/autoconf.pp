58e748adf1518968a684514528364cd020007966
Pedro González Serrano <niteman.es@gmail.com>
2013-12-27 15:18:18 +0100

    manging exclusions

--- manifests/node/autoconf.pp
+++ manifests/node/autoconf.pp
@@ -1,22 +1,30 @@
-class munin::node::autoconf (
-  $avoid = [],
-) {
+class munin::node::autoconf () {
 
   require munin::node::params
 
-  if $avoid == [] {
-    $filter = ''
-  }
-  else {
-    $filter = inline_template('<% avoid.each do |item| %>| sed "\@<%= item %>@d"<% end%>')
-  }
+  if $munin::node::params::autoconf {
+    # TODO: add concat as dependency
+    $filter_file = '/tmp/munin_autoconf_filter'
 
-  exec {"munin-node-configure":
-    #refreshonly => true,
-    command     => "munin-node-configure --shell ${filter} | sh",
-    unless      => "[ $(munin-node-configure --shell ${filter} 2> /dev/null | wc -l) = 0 ]",
-    path        => ["/usr/bin", "/usr/sbin", "/bin"],
-    notify      => Service[$munin::node::params::service_name],
+    concat{ 'munin_node_autoconf_excl' :
+      path => $filter_file,
+    }
+
+
+#    if $avoid == [] {
+      $filter = ''
+#    }
+#    else {
+#      $filter = inline_template('<% avoid.each do |item| %>| sed "\@<%= item %>@d"<% end%>')
+#    }
+  
+    exec {"munin-node-configure":
+      #refreshonly => true,
+      command     => "munin-node-configure --shell ${filter} | sh",
+      unless      => "[ $(munin-node-configure --shell ${filter} 2> /dev/null | wc -l) = 0 ]",
+      path        => ["/usr/bin", "/usr/sbin", "/bin"],
+      notify      => Service[$munin::node::params::service_name],
+    }
   }
 
 }
---
+++ manifests/node/autoconf_exclusion.pp
@@ -0,0 +1,14 @@
+define munin::node::autoconf::exclusion () {
+
+  require munin::node::params
+
+  if $munin::node::params::autoconf {
+
+    concat::fragment { "munin_node_autoconf_excl-${name}" :
+      target  => 'munin_node_autoconf_excl',
+      content => "sed '\@${::munin::node::params::scripts_dir}/${name}@d'",
+    }
+
+  }
+
+}
