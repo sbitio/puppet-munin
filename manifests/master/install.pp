a37a84e632dce3d50b58bd81ad4f421f16f0373b
kris <kris@sbit.io>
2022-02-04 13:32:43 +0100

    fix puppet-lint: selector inside resource block for master/install.pp

--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -1,15 +1,19 @@
 class munin::master::install () {
   require munin::master::params
-  
+
+  $_real_install_options = $::lsbdistcodename ? {
+    /(squeeze|wheezy)/ => ['-t', "${::lsbdistcodename}-backports"],
+    default            => undef,
+  }
+
+  $_real_require = $::lsbdistcodename ? {
+    /(squeeze|wheezy)/ => Apt::Source["backports"],
+    default            => undef,
+  }
+
   package { $munin::master::params::package:
     ensure          => $munin::master::package_ensure,
-    install_options => $::lsbdistcodename ? {
-      /(squeeze|wheezy)/ => ['-t', "${::lsbdistcodename}-backports"],
-      default            => undef,
-    },
-    require         => $::lsbdistcodename ? {
-      /(squeeze|wheezy)/ => Apt::Source["backports"],
-      default            => undef,
-    }
+    install_options => $_real_install_options,
+    require         => $_real_require
   }
 }
