bec8c16c493d46c70371d3f09ea35822861113b0
kris <kris@sbit.io>
2022-02-04 14:12:01 +0100

    fix puppet-lint: selector inside resource block for node/plugin/conf.pp

--- manifests/node/plugin/conf.pp
+++ manifests/node/plugin/conf.pp
@@ -9,19 +9,23 @@ define munin::node::plugin::conf (
 
   $conf_file = "${munin::node::params::plugin_conf_dir}/${name}"
 
+  $_real_source = $source ? {
+    ''      => undef,
+    default => $source,
+  }
+
+  $_real_content = $content ? {
+    ''      => template('munin/node/plugin_conf.erb'),
+    default => $content,
+  }
+
   file { $conf_file:
     ensure  => $ensure,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => $source ? {
-      ''      => undef,
-      default => $source,
-    },
-    content => $content ? {
-      ''      => template('munin/node/plugin_conf.erb'),
-      default => $content,
-    },
+    source  => $_real_source,
+    content => $_real_content,
     notify  => Service[$munin::node::params::service_name],
   }
 
